88e511d0aad3ba0f3da6a37f11ef230ed8af99cc
Dima Shirkov <dimashirkov@list.ru>
2023-07-24 17:56:41 +0300

    a site without a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+
 import tableTop from './assets/table-top.jpg'
 import tableBottom from './assets/table-bottom.jpg'
+
+import tableTop320 from './assets/table-top-500.jpg'
+import tableBottom320 from './assets/table-bottom-500.jpg'
+
 import { useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 
@@ -45,7 +50,7 @@ function App() {
 
         <section className='table-image'>
           <img 
-            src={tableTop}
+            src={document.documentElement.clientWidth === 320 ? tableTop320 : tableTop}
             ref={table}
             className='table-image-top'
           />
@@ -66,18 +71,25 @@ function App() {
           </div>
           
           <img 
-            src={tableBottom}
+            src={document.documentElement.clientWidth === 320 ? tableBottom320 : tableBottom}
             className='table-image-bottom'
           />
         </section>
 
         <section className='page-text'>
-          <h1 className='page-text-heading'>Эргономика — ключ к успеху современного человека</h1>
+
+          <h1 className='page-text-heading'>Эргономика — ключ к успеху современного человека</h1>
+
           <p className='page-text-content'>
-            Cтильная столешница , выполненной из экологически безопасного материала (ДСП), и усиленной стальной рамы не составит труда разместить на поверхности инновационного стола большое количество крупногабаритной техники: стол выдерживает нагрузку до 80 кг.
+            Cтильная столешница , выполненной из экологически безопасного материала (ДСП), и усиленной стальной рамы не составит труда разместить на поверхности инновационного стола большое количество крупногабаритной техники: стол выдерживает нагрузку до 80 кг.
           </p>
-          <p className='page-text-content'>
-            Также столешница, обладающая меламиновым покрытием, хорошо переносит механические воздействия (царапины или удары), при контакте с водой не разрушается, устойчиво переносит бытовые химические вещества и не выгорает под воздействием ультрафиолета.
+
+        </section>
+
+        <section className='page-text-extension'>
+
+          <p className='page-text-content-extension'>
+            Также столешница, обладающая меламиновым покрытием, хорошо переносит механические воздействия (царапины или удары), при контакте с водой не разрушается, устойчиво переносит бытовые химические вещества и не выгорает под воздействием ультрафиолета.
           </p>
 
         </section>
